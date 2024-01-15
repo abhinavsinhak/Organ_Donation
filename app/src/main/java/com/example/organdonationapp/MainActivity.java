b55dc36f319f2e773e970ce3221f8c5d69c5e642
abhinavsinhak <shushantgaurav2005@gmail.com>
2024-01-15 23:48:10 +0530

    new

--- app/src/main/java/com/example/organdonationapp/MainActivity.java
+++ app/src/main/java/com/example/organdonationapp/MainActivity.java
@@ -2,55 +2,123 @@ package com.example.organdonationapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.hbb20.CountryCodePicker;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText editTextName;
-    private EditText editTextLocation;
-    private EditText editTextPassword;
+    private EditText editTextMobile;
+    private EditText editTextOTP;
+    private Button buttonGetOTP;
     private Button buttonRegister;
+    private CountryCodePicker countryCodePicker;
+    private String verificationId;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mAuth = FirebaseAuth.getInstance();
         editTextName = findViewById(R.id.editTextName);
-        editTextLocation = findViewById(R.id.editTextLocation);
-        editTextPassword = findViewById(R.id.editTextPassword);
+        editTextMobile = findViewById(R.id.editTextMobile);
+        editTextOTP = findViewById(R.id.editTextOTP);
+        buttonGetOTP = findViewById(R.id.buttonGetOTP);
         buttonRegister = findViewById(R.id.buttonRegister);
+        countryCodePicker = findViewById(R.id.countryCodePicker);
+        countryCodePicker.registerCarrierNumberEditText(editTextMobile);
 
-        buttonRegister.setOnClickListener(view -> {
-            String name = editTextName.getText().toString();
-            String location = editTextLocation.getText().toString();
-            String password = editTextPassword.getText().toString();
+        buttonGetOTP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String phoneNumber = countryCodePicker.getFullNumberWithPlus().replace(" ", "");
+                sendVerificationCode(phoneNumber);
+            }
+        });
 
-            // Create an Intent to start MainActivity2
-            Intent intent = new Intent(MainActivity.this, MainActivity2.class);
-
-            // Create a Bundle to store the name
-            Bundle bundle = new Bundle();
-            bundle.putString("name", name);
-
-            // Add the Bundle to the Intent
-            intent.putExtras(bundle);
-
-            // Start MainActivity2 with the Intent
-            startActivity(intent);
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String code = editTextOTP.getText().toString();
+                if (code.isEmpty() || code.length() < 6) {
+                    editTextOTP.setError("Enter valid code");
+                    editTextOTP.requestFocus();
+                } else {
+                    verifyCode(code);
+                }
+            }
         });
     }
-    protected void onDestroy() {
 
-        editTextName = null;
-        editTextLocation = null;
-        editTextPassword = null;
-        buttonRegister = null;
+    private void sendVerificationCode(String number) {
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                number,
+                60,
+                TimeUnit.SECONDS,
+                this,
+                new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                    @Override
+                    public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                        super.onCodeSent(s, forceResendingToken);
+                        verificationId = s;
+                        editTextOTP.setVisibility(View.VISIBLE);
+                    }
 
-        super.onDestroy();
+                    @Override
+                    public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                        final String code = phoneAuthCredential.getSmsCode();
+                        if (code != null) {
+                            editTextOTP.setText(code);
+                            verifyCode(code);
+                        }
+                    }
+
+                    @Override
+                    public void onVerificationFailed(@NonNull FirebaseException e) {
+                        Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
+    }
+
+    private void verifyCode(String code) {
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId, code);
+        signInWithCredential(credential);
+    }
+
+    private void signInWithCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                            intent.putExtra("name", editTextName.getText().toString());
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            Toast.makeText(MainActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
 }
